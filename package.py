1ea0231e7b6fe8af992b16f314405470e112075a
Marcelo Sercheli <msercheli@gmail.com>
2020-11-19 18:54:34 -0500

    Add rez files

---
+++ package.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+name = "libpng"
+
+version = "1.6.38"
+
+authors = ["Lib PNG"]
+
+requires = [
+    "zlib",
+]
+
+def commands():
+    #env.PATH.prepend("{root}/bin")
+    #env.LD_LIBRARY_PATH.append("{root}/lib")
+    pass
+
+uuid = "repository.libpng"
