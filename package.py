6e2e40f9b0b69588c8132cdba0a754de98688ea0
Marcelo Sercheli <msercheli@gmail.com>
2021-10-01 12:13:06 -0400

    Add FindPNG.cmake module

--- package.py
+++ package.py
@@ -7,7 +7,7 @@ authors = [
     "Glenn Randers-Pehrson",
 ]
 
-version = "1.6.29"
+version = "1.6.29.sse.1.0.0"
 
 description = \
     """
@@ -42,13 +42,28 @@ variants = [
 tools = [
 ]
 
+uuid = "repository.libpng"
 
 def pre_build_commands():
     command("source /opt/rh/devtoolset-6/enable")
 
 def commands():
+    # NOTE: REZ package versions can have ".sse." to separate the external
+    # version from the internal modification version.
+    split_versions = str(version).split(".sse.")
+    external_version = split_versions[0]
+    internal_version = None
+    if len(split_versions) == 2:
+        internal_version = split_versions[1]
+
+    env.LIBPNG_VERSION = external_version
+    env.LIBPNG_PACKAGE_VERSION = external_version
+    if internal_version:
+        env.LIBPNG_PACKAGE_VERSION = internal_version
+
     env.PNG_ROOT = "{root}"
     env.PNG_LOCATION = "{root}"
     env.PATH.append("{root}/bin")
 
-uuid = "repository.libpng"
+    if building:
+        env.CMAKE_MODULE_PATH.append("{root}/cmake")
